1c2281cfa6fc2b9c29848d2eae35b4de027eb8a1
MostafaYahya773 <mostafayahya822@gmail.com@users.noreply.github.com>
2025-08-23 11:42:42 +0300

    edit in footer

--- src/app/Payment/page.jsx
+++ src/app/Payment/page.jsx
@@ -14,7 +14,7 @@ export default function Payment() {
   // console.log();
   if (CartDetails?.numOfCartItems === 0) {
     return (
-      <div className="w-full mb-150 lg:mb-80 h-screen font-roboto flex flex-col gap-20 justify-center items-center px-10 text-center">
+      <div className="w-full  h-screen font-roboto flex flex-col gap-20 justify-center items-center px-10 text-center">
         <div className="relative w-200 h-200 md:w-300 md:h-300">
           <Image
             src="/noPayment.png"
@@ -34,7 +34,7 @@ export default function Payment() {
   }
   if (isLoading) return <LoadingAnimation />;
   return (
-    <div className="mt-50 md:mt-80 grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-20 ">
+    <div className="mt-50 md:mt-80 mb-150 lg:mb-80 grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-20 ">
       <div className="paymentDetails flex flex-col gap-y-10">
         <div className="address">
           <AddressInPayment />
